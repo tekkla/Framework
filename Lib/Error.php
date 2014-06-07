4aaad1add95b145862c3f49e8fd7abc5e85f4e56
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-06-07 13:52:44 +0200

    Optimizations Removed unused alert() method Changed and commented setter/getter for redirect url Removed obsolete $trace parameter from constructor and error creation.

--- Lib/Error.php
+++ Lib/Error.php
@@ -15,74 +15,75 @@ if (!defined('WEB'))
  */
 class Error extends \Exception
 {
-	private $redirectUrl = null;
+	private $redirectUrl = false;
 
-    // Redefine the exception so message isn't optional
-    public function __construct($message = '', $code = 0, Error $previous = null, $trace = false)
+    /**
+     * Constructor
+     * @param string $message
+     * @param number $code
+     * @param Error $previous
+     * @param string $trace
+     */
+    public function __construct($message = '', $code = 0, Error $previous = null)
     {
+    	// On empty message the default error txt will be used
+    	// and error tracing will be activated
         if (!$message)
             $message = Txt::get('web_error');
 
+        // Errors can habe two different errormessages in form of an array.
+        // The first entry with text for admin users
+        // The second entry with text for normal useser
         if (is_array($message))
         	$message = User::isAdmin() ? $message[0] : $message[1];
 
-        if ($trace && User::isAdmin())
-        {
-        	ob_start();
-        	var_dump(debug_backtrace());
-        	$message .= ob_end_flush();
-        }
-            // make sure everything is assigned properly
         parent::__construct($message, $code, $previous);
     }
 
-    // custom string representation of object
+    /**
+     * (non-PHPdoc)
+     * @see Exception::__toString()
+     */
     public function __toString()
     {
-        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+        return $this->getComplete();
     }
 
+    /**
+     * Returns a Bootstrap formatted error message
+     * @return string
+     */
     public function getComplete()
     {
-        $msg = '
+        $message = '
     	<div class="alert alert-danger">
     		<p>' . $this->getMessage() . '</p>';
 
+        // Append more informations for admin users
         if (User::isAdmin())
         {
-            $msg .= '
-    		<hr>
+            $message .= '
        		<p>In file: ' . $this->getFile() . ' (Line: ' . $this->getLine() . ')</p>
-    		<div style="max-height: 250px; overflow-y: scroll;">
+    		<div style="max-height: 350px; overflow-y: scroll;">
     			<pre>' . $this->getTraceAsString() . '</pre>
     		</div>';
         }
 
-        $msg .= '
+        $message .= '
     	</div>';
 
-        return $msg;
+        return $message;
     }
 
-    public function alert($msg, $title = null, $icon = null)
-    {
-        if (!isset($title))
-            $title = Txt::get('web_error_alert_title');
-
-        if (!isset($icon))
-            $icon = 'alert';
-
-        $cfg = Cfg::create();
-
-        $html = '
-    	<div id="web_alert" title="' . $title . '">
-   			<img src="' . $cfg->get('Web', 'url_images') . '/system/icons/' . $icon . '.png">
-			<p>' . $msg . '
-    	</div>';
-
-        return $html;
-    }
-
+    /**
+     * Hook on SMF error log
+     * @param unknown $message
+     * @param unknown $error_type
+     * @param unknown $error_level
+     * @param unknown $file
+     * @param unknown $line
+     * @throws Error
+     */
     public static function analyzeError($message, $error_type, $error_level, $file, $line)
     {
         if (stripos($message, 'Web') === false)
@@ -93,15 +94,35 @@ class Error extends \Exception
         Throw new Error($message . '<div style="max-height: 250px; overflow-y: scroll;"><pre>' . print_r(debug_backtrace(), true) . '</pre></div>');
     }
 
+    /**
+     * Sets an Url to use a redirect target on error handling
+     * @package string|Url $url String or Url object
+     * @return Error
+     */
     protected function setRedirectUrl($url)
     {
+    	if ($url instanceof Url)
+    		$url = $url->getUrl();
+
     	$this->redirectUrl = $url;
     	return $this;
     }
 
+    /**
+     * Returns the redirect url value
+     */
     public function getRedirectUrl()
     {
-    	return $this->redirectUrl;
+    	return $this->hasRedirectUrl() ? $this->redirectUrl : false;
+    }
+
+    /**
+     * Checks for set redirect url
+     * @return boolean
+     */
+    public function hasRedirectUrl()
+    {
+    	return $this->redirectUrl !== false;
     }
 }
 ?>
