8bb28ba012d68c4e2c169c8677795a60a69147d2
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-08-08 00:13:34 +0200

    New ConfigError handler class
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

---
+++ Lib/Errors/ConfigError.php
@@ -0,0 +1,23 @@
+<?php
+namespace Web\Framework\Lib\Errors;
+
+use Web\Framework\Lib\Abstracts\ErrorAbstract;
+
+// Check for direct file access
+if (!defined('WEB'))
+    die('Cannot run without WebExt framework...');
+
+/**
+ * General error handling object
+ * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
+ * @package WebExt
+ * @subpackage Lib\Errors
+ * @license BSD
+ * @copyright 2014 by author
+ */
+final class ConfigError extends ErrorAbstract
+{
+    protected $fatal = true;
+}
+?>
+
