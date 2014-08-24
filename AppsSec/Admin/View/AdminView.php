ad6b3845a76c29db3a9cfdc90e0532f736e4555c
Michael 'Tekkla' Zorn <tekkla@tekkla.de>
2014-08-25 00:26:12 +0200

    Switched from return to echo.
    Signed-off-by: Michael 'Tekkla' Zorn <tekkla@tekkla.de>

--- AppsSec/Admin/View/AdminView.php
+++ AppsSec/Admin/View/AdminView.php
@@ -15,31 +15,27 @@ class AdminView extends View
 
 	public function Index()
 	{
-		$html = '<h1>WebExt Framework Config</h1>';
+		echo '<h1>WebExt Framework Config</h1>';
 
-		$html .= '
+		echo '
 		<div class="row">
 			<div class="col-sm-6">
 				<div class="panel panel-default">
 					<div class="panel-body">
-						<a class="btn btn-default" href="'. $this->web_config . '">Framework Config</a>
+						<a class="btn btn-default" href="', $this->web_config, '">Framework Config</a>
 						<h3><strong>Applications:</strong></h3>
 						<ul class="list-group">';
-						
-						foreach ($this->loaded_apps as $app_name => $app)
-							$html .= '
-							<li class="list-group-item clearfix">' . $app_name . ($app->config_link ? '<a href="' . $app->config_link . '" class="btn btn-default btn-xs pull-right"><i class="fa fa-cog"></i></a></li>' : '');
 
-						$html .= '
-						</ul>								
+						foreach ($this->loaded_apps as $app_name => $app)
+							echo '
+							<li class="list-group-item clearfix">', $app_name, ($app->config_link ? '<a href="' . $app->config_link . '" class="btn btn-default btn-xs pull-right"><i class="fa fa-cog"></i></a></li>' : '');
+
+						echo '
+						</ul>
 					</div>
 				</div>
 			</div>
 		</div>';
-
-		return $html;
 	}
-
 }
-
 ?>
